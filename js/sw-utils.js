3f3eecbe7eab65adbdc50d3d3f52656a5f1b666c
Alan Piñon <pinhoneibor32@gmail.com>
2022-02-08 14:31:53 -0600

    Proyecto Twittor

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+function updateCacheDynamic(dynamicCache, request, response) {
+
+    if(response.ok){
+        return caches.open(dynamicCache).then(cache => {
+
+            cache.put( request, response.clone() );
+            return response.clone();
+
+        })
+    } else {
+        return response;
+    }
+
+}
